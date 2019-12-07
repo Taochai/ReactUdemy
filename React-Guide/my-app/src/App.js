a1e44ea34a427eb4cb02e77602557c4d1e56f78c
Taochai <42437872+Taochai@users.noreply.github.com>
2019-12-07 13:38:22 -0500

    38

--- React-Guide/my-app/src/App.js
+++ React-Guide/my-app/src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './App.css';
 import Person from './Person/Person';
-function App() {
-  return (
-    <div className="App">
-        <h1>Hi,I`m a React App</h1>
-        <p>This is really working!</p>
-        <Person name="Eric" age={25}> This is props children </Person>
-        <Person/>
-        <Person/>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    name:"Luna",
+    age: 26
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <h1>Hi,I`m a React App</h1>
+          <p>This is really working!</p>
+          <button>Switch Name</button>
+          <Person name="Eric" age={25}> This is props children </Person>
+          <Person name={this.state.name} age={this.state.age}/>
+          <Person/>
+        </div>
+    );
+  }
 }
 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//         <h1>Hi,I`m a React App</h1>
+//         <p>This is really working!</p>
+//         <button>Switch Name</button>
+//         <Person name="Eric" age={25}> This is props children </Person>
+//         <Person/>
+//         <Person/>
+//     </div>
+//   );
+// }
+//
+// export default App;
--- React-Guide/my-app/src/Person/Person.js
+++ React-Guide/my-app/src/Person/Person.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
+
+
+
 const person = (props)=>{
     //JSX中，如果要用javascript， 需要用{}
+    // /*
+    //     在两个tag之间的内容是props.children,可以放一些复杂的html
+    //  */
     return (
         <div>
             <p>I am {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-            // /*
-            //     在两个tag之间的内容是props.children,可以放一些复杂的html
-            //  */
+
         </div>
     )
 }
