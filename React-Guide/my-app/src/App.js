23b0a6808ba61df15e192f1b07f02f4bd575e800
Taochai <42437872+Taochai@users.noreply.github.com>
2019-12-07 17:56:16 -0500

    Update App.js

--- React-Guide/my-app/src/App.js
+++ React-Guide/my-app/src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // state 只能通过this.setState()改变
   state = {
     name:"Luna",
     age: 26
   };
+  setName = ()=>{
+    this.setState({
+      name: 'Edward'
+    })
+  };
+
 
   render() {
     return (
         <div className="App">
           <h1>Hi,I`m a React App</h1>
           <p>This is really working!</p>
-          <button>Switch Name</button>
+          <button onClick={this.setName}>Switch Name</button>
           <Person name="Eric" age={25}> This is props children </Person>
           <Person name={this.state.name} age={this.state.age}/>
           <Person/>
