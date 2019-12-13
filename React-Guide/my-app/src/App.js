d7a10b02c5c07912f992bebac779bbd38ca68603
Taochai <42437872+Taochai@users.noreply.github.com>
2019-12-13 17:27:01 -0500

    package.json

--- React-Guide/my-app/src/App.js
+++ React-Guide/my-app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component{
 
   switchNameHandler = (newAge)=>{
     this.setState({
-      name: 'Luna',
+      name: 'Lunabbbb',
       age:newAge
     })
   };
@@ -24,26 +24,13 @@ class App extends Component{
           <h1>Hi,I`m a React App</h1>
           <p>This is really working!</p>
           <button onClick={this.switchNameHandler.bind(this,50)}>Switch Name</button>
-          <Person click={this.switchNameHandler.bind(this,60)}  name="Eric" age={25}> This is props children </Person>
+          <Person click={this.switchNameHandler.bind(this,60)}  name="Eric" age={25}>This is props children </Person>
           <Person name={this.state.name} age={this.state.age}/>
-          {/*<Person/>*/}
+          <Person/>
         </div>
     );
   }
 }
 
 export default App;
-// function App() {
-//   return (
-//     <div className="App">
-//         <h1>Hi,I`m a React App</h1>
-//         <p>This is really working!</p>
-//         <button>Switch Name</button>
-//         <Person name="Eric" age={25}> This is props children </Person>
-//         <Person/>
-//         <Person/>
-//     </div>
-//   );
-// }
-//
-// export default App;
+
